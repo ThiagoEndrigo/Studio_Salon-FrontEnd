c1aba355838b8b5a943894644309c5e97c93064c
ThiagoEndrigo <116767258+ThiagoEndrigo@users.noreply.github.com>
2026-05-15 09:26:18 -0300

    Primeiro

---
+++ src/api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5001/api",  // <--- TEM QUE SER 5001
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+// Interceptor para adicionar token
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("salon_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    // Adicionar um timestamp para evitar cache em requisições GET que podem falhar na primeira vez
+    if (config.method === 'get') {
+      config.params = { ...config.params, _t: Date.now() };
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
